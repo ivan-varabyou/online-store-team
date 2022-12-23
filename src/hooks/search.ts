f397de4d633fa4e2663b7dca6b7c958abbd2629d
Ivan Varabyou <onebelbiz@gmail.com>
2022-12-24 00:27:40 +0300

    refactor: put the functions in a separate file

---
+++ src/hooks/search.ts
@@ -0,0 +1,27 @@
+import { IResultProduct } from '../models';
+
+export function searchProduct(
+  result: IResultProduct[],
+  search: string,
+): IResultProduct[] {
+  const searchText = search.toLowerCase();
+  return result.filter((product: IResultProduct) => {
+    if (
+      product.title &&
+      product.description &&
+      product.brand &&
+      product.price &&
+      product.stock
+    ) {
+      if (
+        product.title.toLowerCase().includes(searchText) ||
+        product.description.toLowerCase().includes(searchText) ||
+        product.brand.toLowerCase().includes(searchText) ||
+        String(product.price).includes(searchText) ||
+        String(product.stock).includes(searchText)
+      )
+        return true;
+    }
+    return false;
+  });
+}

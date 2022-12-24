8e0ed46b6541cbd845a86796ae135999253d5e8d
Ivan Varabyou <onebelbiz@gmail.com>
2022-12-25 00:37:05 +0300

    feat: add base UI product page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,10 @@
-import React, {useContext, useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import { useHref, useParams } from 'react-router-dom';
 import { useProduct } from '../hooks/useProduct';
 import { IResultProduct } from '../models';
 import { Link } from 'react-router-dom';
 import styles from './../scss/page/ProductPage.module.scss';
 import { CartContext } from '../App';
-
 
 export function ProductPage() {
   const { productId } = useParams();
@@ -13,7 +12,6 @@ export function ProductPage() {
   console.log(result, error, loading);
   if (result !== null) {
     const title = result.title;
-
   }
   const addProductsCart = useContext(CartContext).addProductsCart;
   const removeProductCart = useContext(CartContext).removeProductCart;
@@ -24,99 +22,157 @@ export function ProductPage() {
   const updateCartCountAndSumm = useContext(CartContext).updateCartCountAndSumm;
 
   const dafaultStatusAddToCart =
-  isAddCart && productId && isAddCart(Number(productId));
-const [statusAddToCart, setStatusAddToCart] = useState(
-  dafaultStatusAddToCart,
-);
-const buttonAddToCartText = statusAddToCart ? 'Remove' : 'Add to cart';
-const handleButtonCart = (): void => {
-  if (statusAddToCart) {
-    removeProductCart && removeProductCart(Number(productId), 1);
-    setStatusAddToCart(false);
-  } else {
-    result && addProductsCart && addProductsCart(result, 1);
-    setStatusAddToCart(true);
-  }
-  updateCartCountAndSumm && updateCartCountAndSumm();
-};
+    isAddCart && productId && isAddCart(Number(productId));
+  const [statusAddToCart, setStatusAddToCart] = useState(
+    dafaultStatusAddToCart,
+  );
+  const buttonAddToCartText = statusAddToCart ? 'Remove' : 'Add to cart';
+  const handleButtonCart = (): void => {
+    if (statusAddToCart) {
+      removeProductCart && removeProductCart(Number(productId), 1);
+      setStatusAddToCart(false);
+    } else {
+      result && addProductsCart && addProductsCart(result, 1);
+      setStatusAddToCart(true);
+    }
+    updateCartCountAndSumm && updateCartCountAndSumm();
+  };
 
   return (
     <>
-    {
-      result !== null && 
-      
-      <main className='container'>
-        <div className={styles.productPage}>
-          <div className={styles.ProductPage__title}>
-            <h1>{result.title}</h1>
+      {result !== null && (
+        <main className='container'>
+          <div className='row mt-3'>
+            <div className={styles.productPage__breadcrumb}>
+              <ol className='breadcrumb mb-0'>
+                <li className='breadcrumb-item'>
+                  <Link to='/'>Catalog</Link>
+                </li>
+                <li className='breadcrumb-item'>
+                  <span>{result.category}</span>
+                </li>
+                <li className='breadcrumb-item'>
+                  <span>{result.title}</span>
+                </li>
+                <li className='breadcrumb-item active'>Items</li>
+              </ol>
+            </div>
           </div>
-          <div className={styles.productPage__imageBlock}>
-            <div
-              className={styles.productPage__imgContainer}
-              style={{ backgroundImage: `url(${result.thumbnail})` }}></div>
-          </div>
-          <div className={styles.productPage__infoBlock}>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Description:
-                <span>{result.description}</span>
-              </p>
-            </div>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Discount Percentage:
-                <span>{result.discountPercentage}</span>
-              </p>
-            </div>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Rating:
-                <span>{result.rating}</span>
-              </p>
-            </div>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Stock:
-                <span>{result.stock}</span>
-              </p>
-            </div>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Brand:
-                <span>{result.brand}</span>
-              </p>
-            </div>
-            <div className={styles.productPage__infoItem}>
-              <p className={styles.productPage__infoText}>
-                Category:
-                <span>{result.category}</span>
-              </p>
-              <div className={styles.productPage__buttonGroup}>
-                <div className={styles.productPage__buttons}>
-                  <Link to='/'>
-                    <button
-                      type='button'
-                      className='btn btn-secondary '
-                      data-bs-dismiss='container'>
-                      back
-                    </button>
-                  </Link>
+
+          <div className='row mt-2 mb-8'>
+            <div className='col-lg-6'>
+              <div className='gallery-wrap gallery-vertical'>
+                <div className={styles.productPage__wrapperImage + ' mb-2'}>
+                  <img
+                    src={result.thumbnail}
+                    className={styles.productPage__image}
+                  />
                 </div>
-                <div className={styles.productPage__buttons}>
-                  <button onClick = {handleButtonCart}
-                    type='button'
-                    className='btn btn-primary productPage__buttons'>
-                    {buttonAddToCartText}
-                    
-                  </button>
+
+                <div className={styles.productPage__imagesList + '  mb-3'}>
+                  <div className={styles.productPage__imageItem}>
+                    <img
+                      src={result.thumbnail}
+                      className={styles.productPage__imageItemImg}
+                    />
+                  </div>
+                  <div className={styles.productPage__imageItem}>
+                    <img
+                      src={result.thumbnail}
+                      className={styles.productPage__imageItemImg}
+                    />
+                  </div>
+                  <div className={styles.productPage__imageItem}>
+                    <img
+                      src={result.thumbnail}
+                      className={styles.productPage__imageItemImg}
+                    />
+                  </div>
+                  <div className={styles.productPage__imageItem}>
+                    <img
+                      src={result.thumbnail}
+                      className={styles.productPage__imageItemImg}
+                    />
+                  </div>
+                  <div className={styles.productPage__imageItem}>
+                    <img
+                      src={result.thumbnail}
+                      className={styles.productPage__imageItemImg}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <div className='col-lg-6'>
+              <article className='ps-lg-3'>
+                <h1 className='title text-dark'>{result.title}</h1>
+
+                <div className='mb-3'>
+                  <div className={styles.productPage__infoBlock}>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Description:
+                        <span>{result.description}</span>
+                      </p>
+                    </div>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Discount Percentage:
+                        <span>{result.discountPercentage}</span>
+                      </p>
+                    </div>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Rating:
+                        <span>{result.rating}</span>
+                      </p>
+                    </div>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Stock:
+                        <span>{result.stock}</span>
+                      </p>
+                    </div>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Brand:
+                        <span>{result.brand}</span>
+                      </p>
+                    </div>
+                    <div className={styles.productPage__infoItem}>
+                      <p className={styles.productPage__infoText}>
+                        Category:
+                        <span>{result.category}</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className='mb-4'>
+                  <div className='price h5'>${result.price}</div> <span> </span>
+                </div>
+
+                <div className='row gx-2 mb-4'>
+                  <div className='col-2'>
+                    <select className='form-select'>
+                      <option> 1 </option>
+                      <option> 2 </option>
+                      <option> 3 </option>
+                    </select>
+                  </div>
+
+                  <div className='col-auto'>
+                    <a
+                      onClick={handleButtonCart}
+                      className='btn btn-primary w-100'>
+                      {buttonAddToCartText}
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
           </div>
-        </div>
-      </main>
-    
-    }
+        </main>
+      )}
     </>
   );
 }

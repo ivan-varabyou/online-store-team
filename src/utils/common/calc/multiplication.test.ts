8528f7713a02b7a8d0c26ae0f3e6fa30184c6ac7
Ivan Varabyou <onebelbiz@gmail.com>
2023-01-04 15:43:14 +0300

    refactor: edit test code

--- src/utils/common/calc/multiplication.test.ts
+++ src/utils/common/calc/multiplication.test.ts
@@ -5,9 +5,3 @@ test('Multiplication should return the miltiplication of two numbers', () => {
   expect(multiplication(15, 25)).toBe(375);
   expect(multiplication(11556844, 669855)).toBe(7741409737620);
 });
-
-test('Multiplication ыhould return NaN when multiplying a number by string or undefined', () => {
-  expect(multiplication(5, 'string')).toBeNaN();
-  expect(multiplication(15, 'string')).toBeNaN();
-  expect(multiplication(11556844, undefined)).toBeNaN();
-});
